a25680bdde3ba2377c7a00dfcb2148ca49dc7c0f
Andre Rocha <andrefmrocha@live.com.pt>
2018-12-12 15:11:16 +0000

    Added another exam exercise

--- Tests/Exame 2013-2014 1ª fase v2.py
+++ Tests/Exame 2013-2014 1ª fase v2.py
@@ -34,4 +34,23 @@ def newton(x, func, func_dev, error):
         print(x, func(x))
         x = prev_x - func(prev_x)/func_dev(prev_x)
     return x
-newton(1.8, math_ex_7, math_ex_dev_7, 10**(-5))
+# newton(1.8, math_ex_7, math_ex_dev_7, 10**(-5))
+
+def exercise_6(x):
+    return x + pow(x-2, 2)/(math.sin(x) + 4)
+
+def aurea(x1, x2, func, error):
+    B = (math.sqrt(5) - 1)/2
+    A = B * B
+    counter = 0
+    while abs(x1 - x2) > error and counter < 3:
+        x3 = x1 + A * (x2 - x1)
+        x4 = x1 + B * (x2 - x1)
+        counter+=1
+        print(x1, x2, x3, x4, func(x1), func(x2), func(x3), func(x4))
+        if(func(x3) < func(x4)):
+            x2 = x4
+        else:
+            x1 = x3
+    
+# aurea(-1, 1.5, exercise_6, 10**(-3))
